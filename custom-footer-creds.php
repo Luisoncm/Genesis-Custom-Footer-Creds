45a6e6b31e606a3aaeb3ba98a2630ab19ef362e6
Luisoncm <luisoncm@gmail.com>
2017-10-20 15:36:26 +0200

    add comments, erase data

--- custom-footer-creds.php
+++ custom-footer-creds.php
@@ -1,18 +1,20 @@
 <? php 
+//* Do NOT include the opening php tag shown above. Copy the code shown below.
 
 
 
-
-//* Customize the entire footer
+//* Remove the the creds from the footer
 remove_action( 'genesis_footer', 'genesis_do_footer' );
-add_action( 'genesis_footer', 'lc_custom_footer' );
+
+//* Customize the entire footer 
 function lc_custom_footer() {
 
 	echo '<div class="creds"><p>';
 	echo do_shortcode('[footer_copyright before="Copyright "]');
-	echo ' · <a href="http://pacofdez.com/">pacofdez.com</a> · ';
-	echo 'Handcrafted by <a href="http://luiscolome.com/">Luis Colomé</a> from Málaga · ';
+	echo ' · <a href="http://YOURSITE.com/">YOURSITE</a> · ';
+	echo 'Handcrafted by <a href="http://SITENAME.com/">AUTHORNAME</a> · ';
 	echo do_shortcode('[footer_loginout]');
 	echo '</p></div>';
 
 }
+add_action( 'genesis_footer', 'lc_custom_footer' );
